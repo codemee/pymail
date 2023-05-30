fe4362399e20b14cae85230c62188defddec45b7
meebox <meebox@gmail.com>
2023-05-30 23:44:20 +0800

    各欄位全部改從 xls 檔抓

--- sendmail.py
+++ sendmail.py
@@ -6,22 +6,33 @@ from email import encoders
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 src_filename = "創客研習證明.xlsx"
-cource_name = "11/26 用 ESP32 × Arduino IDE 學 AI 機器學習"
-date_str = '1111126'
-id = 'xxx'
-passwd = 'xxxxxxx'
+id = os.getenv('id')
+passwd = os.getenv('passwd')
 
 # 連線並登入旗標的郵件伺服器
+print("開始執行.")
 smtp = smtplib.SMTP('flag.com.tw', 25)
 smtp.login(id, passwd)
+print("已登入郵件伺服器.")
 
 # 開啟研習證明的登錄試算表檔
 wb = load_workbook(src_filename)
 ws = wb.active
+print("已開啟結業證書名冊檔.")
+
+#     1          2      3      4     5   6   7      
+# 參加人姓名	參加人Email	字號	活動名稱	年	月	日
+cource_name = f"{ws.cell(2,6).value:02d}/{ws.cell(2,7).value:02d} {ws.cell(2,4).value}"
+date_str = f'{ws.cell(2,5).value}{ws.cell(2,6).value:02d}{ws.cell(2,7).value:02d}'
 
 # 針對試算表中活頁簿的每一列 (第一列是標題) 寄送研習證明
 for i in range(2, ws.max_row + 1):
+  print(f"第 {i - 1} 筆資料：")
   print(ws.cell(i, 1).value, ws.cell(i, 2).value)
 
   msg = MIMEMultipart('alternative')
@@ -67,3 +78,4 @@ for i in range(2, ws.max_row + 1):
 
 smtp.quit()
 wb.close()
+print("結束程式.")
